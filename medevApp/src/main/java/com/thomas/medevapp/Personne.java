69f8e0c0e3bfbf4862ef17973492ba0f51da707c
CanalThomas <tomacanal@hotmail.fr>
2023-01-24 15:41:55 +0100

    Personne class add

---
+++ medevApp/src/main/java/com/thomas/medevapp/Personne.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.thomas.medevapp;
+
+/**
+ *
+ * @author Thomas
+ */
+public class Personne {
+    private String firstname;
+    private String lastname;
+    private String email;
+
+    /**
+     *
+     * @param firstname
+     * @param lastname
+     * @param email
+     */
+    public Personne(String firstname, String lastname, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getFirstname() {
+        return firstname;
+    }
+
+    /**
+     *
+     * @param firstname
+     */
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getLastname() {
+        return lastname;
+    }
+
+    /**
+     *
+     * @param lastname
+     */
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     *
+     * @param email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
